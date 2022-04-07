06f45c889bad310d1e979804715070068e253a6b
سعود صالح القرني <103121839+Saudqrr@users.noreply.github.com>
2022-04-07 14:42:02 +0300

    Add files via upload

---
+++ py.py
@@ -0,0 +1,23 @@
+#import turtle module
+import turtle 
+wind = turtle.Screen()
+wind .title(" ping pong ")
+wind.bgcolor("black")
+wind.setup(width=800 , height=600)
+wind.tracer(0)
+#madrab1
+
+madrab1 = turtle.Turtle()
+madrab1.speed(0)
+madrab1.shape("square")
+madrab1.color("red")
+madrab1.penup()
+madrab1.goto(-350, 0)
+  
+
+while True :
+  wind.update()
+
+
+  
+
